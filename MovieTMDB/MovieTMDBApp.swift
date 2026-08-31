bff8ef5de12c1052c5aa61587c45c4d31908dc1e
CicekAzizova <azizovacicak@gmail.com>
2026-09-01 00:04:21 +0400

    Initial commit

---
+++ MovieTMDB/MovieTMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTMDBApp.swift
+//  MovieTMDB
+//
+//  Created by Cicek on 31.08.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieTMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
